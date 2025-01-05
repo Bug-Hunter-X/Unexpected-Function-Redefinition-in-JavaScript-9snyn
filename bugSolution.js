f2c710ffb40bbe154710cbfecb6e423b06e4f00b
BugHunter-X <askzenai@gmail.com>
2025-01-05 13:14:04 +0100

    Add bugSolution.js file

---
+++ bugSolution.js
@@ -0,0 +1,6 @@
+// Avoid redefining the function by using a different name or checking if the function exists before defining it. 
+function add(a,b){return a+b;}
+console.log(add(2,3)); //5
+
+function subtract(a,b){return a-b;}
+console.log(subtract(2,3)); //-1
